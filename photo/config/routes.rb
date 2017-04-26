68eb82a88d2b1f585a833abee7d163431136756c
deviltruong <truong21231995@gmail.com>
2017-04-26 16:02:27 +0700

    finish login

--- photo/config/routes.rb
+++ photo/config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :posts
-#root 'pageStatic#index'  
+#root 'posts#index'  
 end
