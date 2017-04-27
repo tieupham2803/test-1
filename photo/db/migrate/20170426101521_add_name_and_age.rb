4e6a756db3a30bae02f236e80ca4435b8e8a3f6e
deviltruong <truong21231995@gmail.com>
2017-04-27 14:45:57 +0700

    update

--- photo/db/migrate/20170426101521_add_name_and_age.rb
+++ photo/db/migrate/20170426101521_add_name_and_age.rb
@@ -1,7 +1,7 @@
 class AddNameAndAge < ActiveRecord::Migration[5.0]
   def change
-  	add_column :users, :name, :string
-  	add_column :users, :age, :integer
-  	add_column :users, :avatar, :string
+  	#add_column :users, :name, :string
+  	#add_column :users, :age, :integer
+  	#add_column :users, :image, :string
   end
 end
--- photo/db/schema.rb
+++ photo/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426101521) do
+ActiveRecord::Schema.define(version: 20170427055040) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20170426101521) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "image"
+    t.integer  "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
