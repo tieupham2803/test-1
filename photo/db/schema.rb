ba1b18f3bff4955fdf8fb6338ba49023c5ac52cc
Long Hai <hai.do@roxwin.com>
2017-05-03 09:33:49 +0700

    Update giao dien

--- photo/db/schema.rb
+++ photo/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427055040) do
+ActiveRecord::Schema.define(version: 20170426101521) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20170427055040) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "image"
     t.integer  "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
